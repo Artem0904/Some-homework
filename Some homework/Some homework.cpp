30882504c39195583218d9a14b23f1e2899c650e
Artem0904 <moonportal708@gmail.com>
2023-02-16 20:53:19 +0200

    Added list and funct print

--- Some homework/Some homework.cpp
+++ Some homework/Some homework.cpp
@@ -1,9 +1,24 @@
 
 #include <iostream>
+#include <algorithm>
+#include <list>
 using namespace std;
 
+template<typename T>
+void printCont(const T& cont, const string& text = "")
+{
+	cout << text << endl;
+	for (auto it = begin(cont); it != end(cont); it++)
+	{
+		cout << "\t" << *it;
+	}
+	cout << endl;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+    list<string> li;
+	printCont(li, "Empty list : ");
+
 }
